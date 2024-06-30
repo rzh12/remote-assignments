b1c9a110ce86f41d34c71cec8b09400ab5321f4e
rzh12 <jrzhenhan@gmail.com>
2024-06-30 21:24:44 +0800

    Update script.js

--- Week-2/Assignment-4/script.js
+++ Week-2/Assignment-4/script.js
@@ -3,4 +3,12 @@ document.addEventListener('DOMContentLoaded', function () {
     welcomeMessage.addEventListener('click', function () {
         welcomeMessage.textContent = 'Have a Good Time!';
     });
+
+    var showMoreButton = document.getElementById('show-more');
+    showMoreButton.addEventListener('click', function () {
+        var hiddenBoxes = document.querySelectorAll('.content-box.hidden');
+        hiddenBoxes.forEach(function (box) {
+            box.classList.remove('hidden');
+        });
+    });
 });
